b45f66da0d00c1a9a27660c06a71e081084fe4d6
Std-Enigma <std.ilia.bahrebar@gmail.com>
2025-01-20 03:04:38 +0330

    use a texture

--- frag.glsl
+++ frag.glsl
@@ -1,9 +1,13 @@
 #version 330 core
 
 in vec4 inputColor;
+in vec2 texCoord;
 out vec4 outColor;
+
+uniform sampler2D uTexture;
 
 void main() {
   // outColor = vec4(0.4, 0.5, 0.9, 1.0);
-  outColor = inputColor;
+  // outColor = inputColor;
+  outColor = texture(uTexture, texCoord);
 }
